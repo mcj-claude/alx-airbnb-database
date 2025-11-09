5a05bac2f2c97729dfc42d1b570249ffc7068646
Mcjclaude-2025 <trustedgadgets2002@gmail.com>
2025-11-10 00:17:41 +0300

    new

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,14 +1,17 @@
 -- INNER JOIN: Retrieve all bookings and the respective users who made those bookings
 SELECT b.*, u.*
 FROM bookings b
-INNER JOIN users u ON b.user_id = u.id;
+INNER JOIN users u ON b.user_id = u.id
+ORDER BY b.id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT p.*, r.*
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id;
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT u.*, b.*
 FROM users u
-FULL OUTER JOIN bookings b ON u.id = b.user_id;
+FULL OUTER JOIN bookings b ON u.id = b.user_id
+ORDER BY u.id, b.id;
